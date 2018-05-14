fd3608e788c415235e89b985d9d26bb1a7e7a389
Martin Zak <martin.zak@salsitasoft.com>
2018-05-14 15:07:42 +0200

    adding options to control some checks.

--- src/importGroupOrderingRule.ts
+++ src/importGroupOrderingRule.ts
@@ -1,7 +1,42 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
 
+const enum OPTIONS {
+  CHECK_DOT = 'check-dot',
+  CHECK_INDEX = 'check-index',
+  CHECK_EMPTY_LINE = 'check-empty-line',
+  CHECK_ORDER = 'check-order-within-group',
+}
+
 export class Rule extends Lint.Rules.AbstractRule {
+  public static metadata: Lint.IRuleMetadata = {
+    ruleName: 'import-group-ordering',
+    description: 'Enforces strict order of groups of imports (libraries, non-local, local). Also provides some other minor import related enforcements.',
+    rationale: 'Helps maintain a readable style in your codebase.',
+    optionsDescription: 'Allow to turn on and off additional checks.',
+    options: {
+      type: 'object',
+      properties: {
+        [OPTIONS.CHECK_DOT]: {
+          type: 'boolean',
+        },
+        [OPTIONS.CHECK_INDEX]: {
+          type: 'boolean',
+        },
+        [OPTIONS.CHECK_EMPTY_LINE]: {
+          type: 'boolean',
+        },
+        [OPTIONS.CHECK_ORDER]: {
+          type: 'boolean',
+        }
+      },
+      additionalProperties: false,
+    },
+    type: 'typescript',
+    typescriptOnly: false,
+    hasFix: false
+  };
+
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     return this.applyWithWalker(new CheckImportsWalker(sourceFile, this.getOptions()));
   }
@@ -22,6 +57,12 @@ interface ImportDeclaration {
 
 class CheckImportsWalker extends Lint.RuleWalker {
   private importStatements: ImportDeclaration[] = [];
+  private userOptions: any;
+
+  constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
+    super(sourceFile, options);
+    this.userOptions = this.getOptions()[0] || {};
+  }
 
   private get lastImport(): ImportDeclaration {
     return this.importStatements[this.importStatements.length - 1] || null;
@@ -30,11 +71,15 @@ class CheckImportsWalker extends Lint.RuleWalker {
   public visitImportDeclaration(node: ts.ImportDeclaration): void {
     const current = this.parseImport(node);
 
-    if (current.moduleText.startsWith('./..')) {
-      this.addImportFailure(node, 'Import must not start with superfluous ./');
+    if (this.userOptions[OPTIONS.CHECK_DOT] !== false) {
+      if (current.moduleText.startsWith('./..')) {
+        this.addImportFailure(node, 'Import must not start with superfluous ./');
+      }
     }
-    if (current.moduleText.endsWith('/index')) {
-      this.addImportFailure(node, 'Import must not end with index');
+    if (this.userOptions[OPTIONS.CHECK_INDEX] !== false) {
+      if (current.moduleText.endsWith('/index')) {
+        this.addImportFailure(node, 'Import must not end with index');
+      }
     }
 
     const last = this.lastImport;
@@ -46,11 +91,15 @@ class CheckImportsWalker extends Lint.RuleWalker {
       if (last.type > ImportType.NonLocal  && current.type === ImportType.NonLocal) {
         this.addImportFailure(node, 'Non-local import must come before Local one');
       }
-      if (last.type !== current.type && !current.node.getFullText().match(/^\r?\n\r?\n/)) {
+      if (this.userOptions[OPTIONS.CHECK_EMPTY_LINE] !== false) {
+        if (last.type !== current.type && !current.node.getFullText().match(/^\r?\n\r?\n/)) {
           this.addImportFailure(node, 'Blocks of Libraries, Local and Non-local imports must be divided by empty line');
+        }
       }
-      if (last.type === ImportType.NonLocal && last.type === current.type && last.depth < current.depth) {
+      if (this.userOptions[OPTIONS.CHECK_ORDER] !== false) {
+        if (last.type === ImportType.NonLocal && last.type === current.type && last.depth < current.depth) {
           this.addImportFailure(node, 'Non-local imports must be ordered from the most distant the closest');
+        }
       }
     }
 
